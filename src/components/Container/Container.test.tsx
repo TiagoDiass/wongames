9cf42800259232181a9525654e86fe3dea2f7a49
TiagoDiass <tiago.costadiasss@gmail.com>
2022-04-07 15:57:17 -0300

    feat: add Container component

---
+++ src/components/Container/Container.test.tsx
@@ -0,0 +1,32 @@
+import Container from './Container';
+import { renderWithTheme } from 'utils/test-utils';
+import theme from 'styles/theme';
+
+describe('Component: Container', () => {
+  it('should render correctly', () => {
+    const { container } = renderWithTheme(
+      <Container>
+        <span>Won Games</span>
+      </Container>
+    );
+
+    expect(container.firstChild).toHaveStyleRule('max-width', theme.grid.container);
+    expect(container.firstChild).toMatchInlineSnapshot(`
+      .c0 {
+        max-width: 130rem;
+        margin-left: auto;
+        margin-right: auto;
+        padding-left: calc(3.2rem / 2);
+        padding-right: calc(3.2rem / 2);
+      }
+
+      <div
+        class="c0"
+      >
+        <span>
+          Won Games
+        </span>
+      </div>
+    `);
+  });
+});
---
+++ src/components/Container/Container.tsx
@@ -0,0 +1,16 @@
+import styled, { css } from 'styled-components';
+
+/**
+ * Component used as a Container to wrap contents
+ */
+const Container = styled.div`
+  ${({ theme }) => css`
+    max-width: ${theme.grid.container};
+    margin-left: auto;
+    margin-right: auto;
+    padding-left: calc(${theme.grid.gutter} / 2);
+    padding-right: calc(${theme.grid.gutter} / 2);
+  `}
+`;
+
+export default Container;
