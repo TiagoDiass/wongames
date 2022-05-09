3da4398582197b521e85d97580c85a518b4f6350
TiagoDiass <tiago.costadiasss@gmail.com>
2022-05-09 17:47:14 -0300

    feat: add huge size to Heading component

--- src/components/Heading/Heading.test.tsx
+++ src/components/Heading/Heading.test.tsx
@@ -3,48 +3,50 @@ import { renderWithTheme } from 'utils/test-utils';
 import { screen } from '@testing-library/react';
 import theme from 'styles/theme';
 
+const getHeading = () => screen.getByRole('heading', { name: 'Won Games' });
+
 describe('Component: Heading', () => {
   it('should render a white heading by default', () => {
     renderWithTheme(<Heading>Won Games</Heading>);
-    expect(screen.getByRole('heading', { name: 'Won Games' })).toHaveStyle({
+    expect(getHeading()).toHaveStyle({
       color: theme.colors.white
     });
   });
 
   it('should render a black heading when specified', () => {
     renderWithTheme(<Heading color='black'>Won Games</Heading>);
-    expect(screen.getByRole('heading', { name: 'Won Games' })).toHaveStyle({
+    expect(getHeading()).toHaveStyle({
       color: theme.colors.black
     });
   });
 
   it('should render a heading with a line on the left side', () => {
     renderWithTheme(<Heading lineLeft>Won Games</Heading>);
-    expect(screen.getByRole('heading', { name: 'Won Games' })).toHaveStyle({
+    expect(getHeading()).toHaveStyle({
       'border-left': `0.7rem solid ${theme.colors.primary}`
     });
   });
 
   it('should render a heading with a line at the bottom', () => {
     renderWithTheme(<Heading lineBottom>Won Games</Heading>);
-    expect(screen.getByRole('heading', { name: 'Won Games' })).toHaveStyleRule(
-      'border-bottom',
-      `0.5rem solid ${theme.colors.primary}`,
-      {
-        modifier: '::after'
-      }
-    );
+    expect(getHeading()).toHaveStyleRule('border-bottom', `0.5rem solid ${theme.colors.primary}`, {
+      modifier: '::after'
+    });
+  });
+
+  it('should render a heading in a huge size', () => {
+    renderWithTheme(<Heading size='huge'>Won Games</Heading>);
+    expect(getHeading()).toHaveStyleRule('font-size', theme.font.sizes.huge);
   });
 
   it('should render a heading in a smaller size', () => {
     renderWithTheme(<Heading size='small'>Won Games</Heading>);
-    expect(screen.getByRole('heading', { name: 'Won Games' })).toHaveStyleRule(
-      'font-size',
-      theme.font.sizes.medium
-    );
+    const heading = getHeading();
+
+    expect(heading).toHaveStyleRule('font-size', theme.font.sizes.medium);
 
     // also checking if line bottom is smaller
-    expect(screen.getByRole('heading', { name: 'Won Games' })).toHaveStyleRule('width', `3rem`, {
+    expect(heading).toHaveStyleRule('width', `3rem`, {
       modifier: '::after'
     });
   });
@@ -56,7 +58,7 @@ describe('Component: Heading', () => {
       </Heading>
     );
 
-    const heading = screen.getByRole('heading', { name: 'Won Games' });
+    const heading = getHeading();
 
     expect(heading).toHaveStyle({
       'border-left': `0.7rem solid ${theme.colors.primary}`
@@ -74,7 +76,7 @@ describe('Component: Heading', () => {
       </Heading>
     );
 
-    const heading = screen.getByRole('heading', { name: 'Won Games' });
+    const heading = getHeading();
 
     expect(heading).toHaveStyle({
       'border-left': `0.7rem solid ${theme.colors.secondary}`
--- src/templates/Auth/Auth.tsx
+++ src/templates/Auth/Auth.tsx
@@ -17,7 +17,7 @@ export default function Auth({ title, children }: AuthProps) {
           <Logo id='banner' />
 
           <div>
-            <Heading>All your favorite games in one place</Heading>
+            <Heading size='huge'>All your favorite games in one place</Heading>
             <S.BannerSubtitle>
               <strong>WON</strong> is the best and most complete gaming platform
             </S.BannerSubtitle>
