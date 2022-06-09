e3dde6e40a5580831279f1648984b6045591ec3f
Tiago Dias <tiago.costadiasss@gmail.com>
2022-06-08 22:48:49 -0300

    chore: add stuff on components/index

--- src/components/index.ts
+++ src/components/index.ts
@@ -18,3 +18,5 @@ export { default as RatingFeedback } from './RatingFeedback/RatingFeedback';
 export { default as Ribbon } from './Ribbon/Ribbon';
 export { default as Slider } from './Slider/Slider';
 export { default as TextField } from './TextField/TextField';
+
+// what if I add a comment just to commit?
