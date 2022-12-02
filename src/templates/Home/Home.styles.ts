b4422ed8da1c31fcda0f0bf222a23d7c5a7accae
TiagoDiass <tiago.costadiasss@gmail.com>
2022-12-01 21:29:18 -0300

    chore: remove useless styled components

--- src/templates/Home/Home.styles.ts
+++ src/templates/Home/Home.styles.ts
@@ -1,66 +1,3 @@
-// import styled, { css } from 'styled-components';
-// import media from 'styled-media-query';
-
-// import * as HeadingStyles from 'components/Heading/Heading.styles';
-// import * as HighlightStyles from 'components/Highlight/Highlight.styles';
-
-// export const SectionBanner = styled.section`
-//   ${({ theme }) => css`
-//     margin: ${theme.spacings.large} calc(-${theme.grid.gutter} / 2);
-
-//     ${media.greaterThan('medium')`
-//       margin: ${theme.spacings.large} 0;
-//       position: relative;
-//       z-index: ${theme.layers.base};
-//     `}
-//   `}
-// `;
-
-// export const SectionNewGames = styled.div`
-//   ${({ theme }) => css`
-//     margin-bottom: calc(${theme.spacings.xxlarge} * 2);
-
-//     ${media.greaterThan('large')`
-//       margin-top: -13rem;
-//     `}
-
-//     ${media.greaterThan('medium')`
-//       margin-bottom: 0;
-//       padding-top: 14rem;
-//       padding-bottom: 10rem;
-//       background-color: ${theme.colors.lightBg};
-//       clip-path: polygon(0 0, 100% 15%, 100% 100%, 0 85%);
-
-//       ${HeadingStyles.Wrapper} {
-//         color: ${theme.colors.black};
-//       }
-//     `}
-//   `}
-// `;
-
-// export const SectionUpcoming = styled.div`
-//   ${({ theme }) => css`
-//     ${HighlightStyles.Wrapper} {
-//       margin-top: calc(${theme.spacings.xlarge} * 2);
-//     }
-//   `}
-// `;
-
-// export const SectionFooter = styled.section`
-//   ${({ theme }) => css`
-//     margin-top: ${theme.spacings.large};
-//     padding-bottom: ${theme.spacings.xsmall};
-//     padding-top: ${theme.spacings.xxlarge};
-//     background-color: ${theme.colors.white};
-//     clip-path: polygon(0 5%, 100% 0%, 100% 100%, 0 100%);
-
-//     ${media.greaterThan('medium')`
-//       padding-top: calc(${theme.spacings.xxlarge} * 2);
-//       clip-path: polygon(0 15%, 100% 0%, 100% 100%, 0 100%);
-//     `}
-//   `}
-// `;
-
 import styled, { css } from 'styled-components';
 import media from 'styled-media-query';
 
@@ -106,20 +43,5 @@ export const SectionUpcoming = styled.div`
     ${HighlightStyles.Wrapper} {
       margin-top: calc(${theme.spacings.xlarge} * 2);
     }
-  `}
-`;
-
-export const SectionFooter = styled.section`
-  ${({ theme }) => css`
-    margin-top: ${theme.spacings.large};
-    padding-bottom: ${theme.spacings.xsmall};
-    padding-top: ${theme.spacings.xxlarge};
-    background-color: ${theme.colors.white};
-    clip-path: polygon(0 5%, 100% 0%, 100% 100%, 0 100%);
-
-    ${media.greaterThan('medium')`
-      padding-top: calc(${theme.spacings.xxlarge} * 2);
-      clip-path: polygon(0 15%, 100% 0%, 100% 100%, 0 100%);
-    `}
   `}
 `;
