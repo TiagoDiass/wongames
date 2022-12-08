49ac8d923bb46d62e681186b17efbc3e537709ab
TiagoDiass <tiago.costadiasss@gmail.com>
2022-12-08 19:11:57 -0300

    test: ensure Gallery render thumbnails as buttons with correct src

--- src/components/Gallery/Gallery.test.tsx
+++ src/components/Gallery/Gallery.test.tsx
@@ -1,8 +1,32 @@
-import Gallery from './Gallery';
+import Gallery, { GalleryImageProps } from './Gallery';
+import { screen } from '@testing-library/react';
 import { renderWithTheme } from 'utils/test-utils';
 
+const images: GalleryImageProps[] = [
+  {
+    src: '/img/games/cyberpunk-1.jpeg',
+    label: 'Gallery Image 1'
+  },
+  {
+    src: '/img/games/cyberpunk-2.jpeg',
+    label: 'Gallery Image 2'
+  }
+];
+
 describe('Component: Gallery', () => {
-  it('should render correctly', () => {
-    renderWithTheme(<Gallery images={[]} />);
+  it('should render thumbnails as buttons correctly with the correct src', () => {
+    renderWithTheme(<Gallery images={images} />);
+
+    expect(screen.getByRole('button', { name: `Thumb - ${images[0].label}` })).toBeInTheDocument();
+    expect(screen.getByRole('button', { name: `Thumb - ${images[0].label}` })).toHaveAttribute(
+      'src',
+      images[0].src
+    );
+
+    expect(screen.getByRole('button', { name: `Thumb - ${images[1].label}` })).toBeInTheDocument();
+    expect(screen.getByRole('button', { name: `Thumb - ${images[1].label}` })).toHaveAttribute(
+      'src',
+      images[1].src
+    );
   });
 });
