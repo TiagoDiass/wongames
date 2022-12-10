ad77cafd8c8b05c2f6a200ca113008184a212d3b
TiagoDiass <tiago.costadiasss@gmail.com>
2022-12-09 23:04:06 -0300

    feat(Game): add game cover (static still)

--- src/templates/Game/Game.tsx
+++ src/templates/Game/Game.tsx
@@ -1,10 +1,10 @@
 import Base from 'templates/Base/Base';
-// import * as S from './Game.styles';
+import * as S from './Game.styles';
 
 export default function Game() {
   return (
     <Base>
-      <h1>Game</h1>
+      <S.Cover role='img' src='/img/games/cyberpunk-6.jpeg' aria-label='Cover' />
     </Base>
   );
 }
